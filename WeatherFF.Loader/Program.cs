6c7c8244171b5d06a37a06432ead49915c684f45
Alex Fomin <ShurickFomin@gmail.com>
2017-04-30 12:55:32 +0300

    Initial commit

---
+++ WeatherFF.Loader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherFF.Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
