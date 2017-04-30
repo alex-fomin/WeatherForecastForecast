b6438eecf8c5de910776ee0611f4c2d7b89f4b24
Alex Fomin <ShurickFomin@gmail.com>
2017-05-01 00:38:12 +0300

    Fix line endings

--- WeatherFF.Common/UtilsExtensions.cs
+++ WeatherFF.Common/UtilsExtensions.cs
@@ -2,17 +2,17 @@
 
 namespace WeatherFF.Common
 {
-	public static class UtilsExtensions
-	{
-		public static double? ParseNullable(this string value)
-		{
-			if (double.TryParse(value, out var result))
-				return result;
-			else
-				return null;
-		}
+    public static class UtilsExtensions
+    {
+        public static double? ParseNullable(this string value)
+        {
+            if (double.TryParse(value, out var result))
+                return result;
+            else
+                return null;
+        }
 
-		public static TValue GetValueOrDefault<T, TValue>(this Dictionary<T, TValue> dictionary, T key,
-			TValue @default = default(TValue)) => dictionary.TryGetValue(key, out var value) ? value : @default;
-	}
+        public static TValue GetValueOrDefault<T, TValue>(this Dictionary<T, TValue> dictionary, T key,
+            TValue @default = default(TValue)) => dictionary.TryGetValue(key, out var value) ? value : @default;
+    }
 }
